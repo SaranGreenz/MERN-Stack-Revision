04b0b3323e323ea539254f960d46072b5f85e888
SaranGreenz <sarangreenz@gmail.com>
2022-06-23 14:53:57 +0530

    1 file modified, 1 added

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const app = require('express')();
 
 app.use(require('express').json());
 app.use(require('cors')());
+require('dotenv').config()
 const port = process.env.PORT || 7000;
 
 app.listen(port, () => {
