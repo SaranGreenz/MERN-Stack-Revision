5585d0e43aada17c4949786be36b39d539fb3427
SaranGreenz <sarangreenz@gmail.com>
2022-06-23 15:03:05 +0530

    1 file modified

--- frontend/tracker/src/App.js
+++ frontend/tracker/src/App.js
@@ -1,10 +1,37 @@
-import {Component} from 'react'
+import { Component } from 'react';
 
-class App extends Component{
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+    };
+  }
+
+  componentDidMount = () => {
+    fetch('https://saran-hogwarts-backend.herokuapp.com/getStudents')
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw response;
+      })
+      .then((data) => {
+        this.setState({
+          data: data,
+        });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
   render() {
     return (
       <>
-        
+        {this.data.map((d) => (
+          <p key={d._id}>{d.Name}</p>
+        ))}
       </>
     );
   }
