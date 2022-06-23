aba6376d991cb63e7349909da5acccdad16c8c25
SaranGreenz <sarangreenz@gmail.com>
2022-06-23 14:53:03 +0530

    1 file added

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const app = require('express')();
+
+app.use(require('express').json());
+app.use(require('cors')());
+const port = process.env.PORT || 7000;
+
+app.listen(port, () => {
+  console.log('Server is running on port: ', port);
+});
